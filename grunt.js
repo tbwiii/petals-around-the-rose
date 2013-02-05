94662711071aa2e2db701afff91cb574bc8b64cb
Ted Waller <ted@tbwiii.com>
2013-02-05 10:40:59 -0500

    cleaned grunt file

--- grunt.js
+++ grunt.js
@@ -4,49 +4,12 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     meta: {
-      version: '0.1.0',
-    },
-
-    lint: {
-      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
-    },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    concat: {
-      dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:lib/FILE_NAME.js>'],
-        dest: 'dist/FILE_NAME.js'
-      }
-    },
-    min: {
-      dist: {
-        src: 'scripts/petals.js',
-        dest: 'scripts/roll.min.js'
-      }
+      version: '0.1.0'
     },
     watch: {
       files: ['sass/screen.scss'],
       tasks: ['compass:dist']
     },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {}
-    },
-    uglify: {},
-
     compass: {                 // Task
       dist: {
         src:  'sass',
@@ -58,6 +21,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-compass');
 
   // Default task.
-  grunt.registerTask('default', 'lint qunit concat min');
+  grunt.registerTask('default', 'watch');
 
 };
